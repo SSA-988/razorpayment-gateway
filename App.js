ba139a44f658fef2a6d0cd713abdd6c2e6815f6e
SSA-988 <sujananand0@gmail.com>
2024-08-11 09:53:41 +0530

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import RazorpayCheckout from "react-native-razorpay";
 
 export default function App() {
+  const openRazorpay = () => {
+    console.log("pressed");
+    const options = {
+      description: "Adding to Wallet",
+      currency: "INR",
+      name: "Eyan",
+      key: "rzp_test_E3GWYimxN7YMk8",
+      amount: "5000",
+      prefill: {
+        email: "void@razorpay.com",
+        contact: "9191919191",
+        name: "Razorpay Software",
+      },
+      theme: { color: "#F37254" },
+    };
+    console.log("Opening", options);
+    RazorpayCheckout.open(options)
+      .then((data) => {
+        // handle success
+        alert(`Success: ${data.razorpay_payment_id}`);
+      })
+      .catch((error) => {
+        // handle failure
+        alert(`Error: ${error.code} | ${error.description}`);
+      });
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Pressable onPress={openRazorpay}>
+        <Text>PAY</Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +42,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
